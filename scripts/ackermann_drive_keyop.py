a9d270b46f70caf63c3a493a2fe96cc833e500c9
George Kouros <gkourosg@yahoo.gr>
2016-01-25 18:07:34 +0200

    Improve state printing and make steps adjustable

--- scripts/ackermann_drive_keyop.py
+++ scripts/ackermann_drive_keyop.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 '''
-ackermann_drive_keyop.py: A ros keyboard teleop script for ackermann robots
+ackermann_drive_keyop.py:
+    A ros keyboard teleoperation script for ackermann steering based robots
 '''
 
-__author__ = "George Kouros"
-__license__ = "GPLv3"
-__maintainer__ = "George Kouros"
-__email__ = "gkourosg@yahoo.gr"
+__author__ = 'George Kouros'
+__license__ = 'GPLv3'
+__maintainer__ = 'George Kouros'
+__email__ = 'gkourosg@yahoo.gr'
 
 import roslib
 import rospy
@@ -16,7 +17,6 @@ from std_msgs.msg import Float64
 import sys, select, termios, tty
 import thread
 from numpy import clip
-from sys import argv
 
 control_keys = {
     'up'    : '\x41',
@@ -24,35 +24,51 @@ control_keys = {
     'right' : '\x43',
     'left'  : '\x44',
     'space' : '\x20',
-    'tab' : '\x09'}
+    'tab'   : '\x09'}
 
-control_bindings = {
-    '\x41' : ( 0.1 , 0.0),
-    '\x42' : (-0.1 , 0.0),
-    '\x43' : ( 0.0 ,-0.1),
-    '\x44' : ( 0.0 , 0.1),
+key_bindings = {
+    '\x41' : ( 1.0 , 0.0),
+    '\x42' : (-1.0 , 0.0),
+    '\x43' : ( 0.0 ,-1.0),
+    '\x44' : ( 0.0 , 1.0),
     '\x20' : ( 0.0 , 0.0),
     '\x09' : ( 0.0 , 0.0)}
 
 
 class AckermannDriveKeyop:
 
-    def __init__(self, max_speed=0.5, max_steering_angle=0.8):
+    def __init__(self, max_speed=0.22, max_steering_angle=0.7):
         self.speed_range = [-float(max_speed), float(max_speed)]
-        self.steering_angle_range = [-float(max_steering_angle), float(max_steering_angle)]
+        self.steering_angle_range = [-float(max_steering_angle),
+                                     float(max_steering_angle)]
+        for key in key_bindings:
+            key_bindings[key] = \
+                    (key_bindings[key][0] * float(max_speed) / 5,
+                     key_bindings[key][1] * float(max_steering_angle) / 5)
+
         self.speed = 0
         self.steering_angle = 0
-        self.motors_pub = rospy.Publisher('ackermann_cmd', AckermannDrive,\
-                queue_size=1)
+        self.motors_pub = rospy.Publisher(
+            'ackermann_cmd', AckermannDrive, queue_size=1)
         rospy.Timer(rospy.Duration(1.0/5.0), self.pub_callback, oneshot=False)
         self.key_loop()
-
 
     def pub_callback(self, event):
         ackermann_cmd_msg = AckermannDrive()
         ackermann_cmd_msg.speed = self.speed
         ackermann_cmd_msg.steering_angle = self.steering_angle
         self.motors_pub.publish(ackermann_cmd_msg)
+
+    def print_state(self):
+        sys.stderr.write('\x1b[2J\x1b[H')
+        rospy.loginfo('\x1b[1M\r*********************************************')
+        rospy.loginfo('\x1b[1M\rUse arrows to change speed and steering angle')
+        rospy.loginfo('\x1b[1M\rUse space to brake and tab to align wheels')
+        rospy.loginfo('\x1b[1M\rPress <ctrl-c> or <q> to exit')
+        rospy.loginfo('\x1b[1M\r*********************************************')
+        rospy.loginfo('\x1b[1M\r\033[32;1mSpeed: %0.2f m/s, '
+                      'Steer Angle: %0.1f rad\033[0m',
+                      self.speed, self.steering_angle)
 
     def get_key(self):
         tty.setraw(sys.stdin.fileno())
@@ -63,32 +79,29 @@ class AckermannDriveKeyop:
 
 
     def key_loop(self):
-        rospy.loginfo("Use the arrow keys to adjust speed or position and space to halt/reset")
-        rospy.loginfo("Press 'ctr-c' or 'q' to exit")
         self.settings = termios.tcgetattr(sys.stdin)
-
         while 1:
-          key = self.get_key()
-          if key in control_bindings.keys():
-              if key == control_keys['space']:
-                  self.speed = 0.0
-              elif key == control_keys['tab']:
-                  self.steering_angle = 0.0
-              else:
-                  self.speed = self.speed + control_bindings[key][0]
-                  self.steering_angle = \
-                          self.steering_angle + control_bindings[key][1]
-              self.speed = \
-                  clip(self.speed, self.speed_range[0], self.speed_range[1])
-              self.steering_angle = \
-                 clip(self.steering_angle,
-                         self.steering_angle_range[0],
-                         self.steering_angle_range[1])
-              rospy.loginfo("Speed: %0.1f, Steering Angle: %0.1f", self.speed, self.steering_angle)
-          elif key == '\x03' or key == '\x71':  # ctr-c or q
-            break
-          else:
-            continue
+            key = self.get_key()
+            if key in key_bindings.keys():
+                if key == control_keys['space']:
+                    self.speed = 0.0
+                elif key == control_keys['tab']:
+                    self.steering_angle = 0.0
+                else:
+                    self.speed = self.speed + key_bindings[key][0]
+                    self.steering_angle = \
+                            self.steering_angle + key_bindings[key][1]
+                    self.speed = clip(
+                        self.speed, self.speed_range[0], self.speed_range[1])
+                    self.steering_angle = clip(
+                        self.steering_angle,
+                        self.steering_angle_range[0],
+                        self.steering_angle_range[1])
+                self.print_state()
+            elif key == '\x03' or key == '\x71':  # ctr-c or q
+                break
+            else:
+                continue
         self.finalize()
 
     def finalize(self):
@@ -100,17 +113,16 @@ class AckermannDriveKeyop:
         self.motors_pub.publish(ackermann_cmd_msg)
         sys.exit()
 
-if __name__ == "__main__":
-  rospy.init_node('ackermann_drive_keyop_node')
-  rospy.loginfo("Ackermann Drive Keyboard Teleoperation Node Initialized")
-  rospy.loginfo("Use arrows to change speed and steering angle")
-  rospy.loginfo("Use space to brake and tab to align wheels")
-  args = argv[1:]
-  if len(args) == 2:
-    rospy.loginfo("Setting motors max speed to %s and steering angle to %s",\
-            args[0], args[1])
-    keyop = AckermannDriveKeyop(args[0], args[1])
-  else:
-    rospy.loginfo("Using default motors max speed(0.5m/s) and max steering"
-            "angle(0.8rad)")
-    keyop = AckermannDriveKeyop()
+if __name__ == '__main__':
+    sys.stderr.write('\x1b[2J\x1b[H')
+    rospy.init_node('ackermann_drive_keyop_node')
+    rospy.loginfo('Ackermann Drive Keyboard Teleoperation Node Initialized')
+    args = sys.argv[1:]
+    if len(args) == 2:
+        rospy.loginfo('Setting motors max speed to %s and steering angle to %s',
+                      args[0], args[1])
+        keyop = AckermannDriveKeyop(args[0], args[1])
+    else:
+        rospy.loginfo('Using default motors max speed(0.5m/s) and steering '
+                      'angle(0.8rad)')
+        keyop = AckermannDriveKeyop()
